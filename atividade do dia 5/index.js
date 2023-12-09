336e1c84f704999f5251cee6803b630644c6f3c9
Davi Barreto <davibarretojkda@gmail.com>
2023-12-09 17:14:17 -0300

    Dia 5 update

---
+++ atividade do dia 5/index.js
@@ -0,0 +1,27 @@
+// criando as variaveis  para realizar aaa operação 
+
+let num1 = parseInt(prompt("Digite o primeiro número"))
+let num2 = parseInt(prompt("Digite o segundo número"))
+let opcao = parseInt(prompt("Escolha a operação que deseja realizar" + "\n1 = +; \n2 = -;\n3 = *;\n4 = /;"))
+
+
+//criando o switch
+
+
+switch(opcao) {
+    case 1:
+        console.log(num1 + " + " + num2 + " = "+ (num1 + num2))
+        break;
+    case 2:
+        console.log(num1 + " - " + num2 + " = "+ (num1 - num2))
+        break;   
+    case 3:
+        console.log(num1 + " * " + num2 + " = "+ (num1 * num2))
+        break;  
+    case 4:
+        console.log(num1 +" / " + num2 + " = " + (num1 / num2))
+        break;
+    default:
+        console.log ("!!! ERRO ESCOLHA UMA OPÇÃO APENAS DE 1 A 4 !!!")
+        break;
+}
